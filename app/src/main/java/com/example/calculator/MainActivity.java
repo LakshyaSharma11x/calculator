78e569267f123b187132c10fb8402a7d03f26aa6
Lakshya Sharma <lakshyasharma11052002@gmail.com>
2024-10-18 22:47:58 +0530

    add design to buttons

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.calculator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,117 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText e;
+    Button B1,B2,B3,B4,B5,B6,B7,B8,B9,B0,B_ADD,B_SUB,B_MUL,B_DIV,B_ANS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        e = findViewById(R.id.e);
+
+        B1 = findViewById(R.id.B1);
+        B2 = findViewById(R.id.B2);
+        B3 = findViewById(R.id.B3);
+
+        B4 = findViewById(R.id.B4);
+        B5 = findViewById(R.id.B5);
+        B6 = findViewById(R.id.B6);
+
+        B7 = findViewById(R.id.B7);
+        B8 = findViewById(R.id.B8);
+        B9 = findViewById(R.id.B9);
+        B0 = findViewById(R.id.B0);
+        B_ADD = findViewById(R.id.A_ADD);
+        B_SUB = findViewById(R.id.B_SUB);
+        B_MUL = findViewById(R.id.B_MUL);
+
+        B_ANS = findViewById(R.id.B_ANS);
+
+        B1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pre=e.getText().toString();
+
+                e.setText("1"+pre);
+            }
+        });
+        B2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pre=e.getText().toString();
+
+                e.setText("2"+pre);
+            }
+        });
+        B3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        B4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B_ADD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B_SUB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        B_MUL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        B_ANS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
         });
     }
+
 }
